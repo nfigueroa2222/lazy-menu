159a6f679ab6710138ab0efaad95ba130b7d8fe0
nfigueroa2222 <nikolasF00@gmail.com>
2025-09-07 22:11:45 -0400

    Auto-generated commit

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app';
-import { routes } from './app/app.routes';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
